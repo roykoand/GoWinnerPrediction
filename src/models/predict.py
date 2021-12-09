808490f4d30d01f13906cf1a7725f027fddbb4ad
roykoand <roykoand@gmail.com>
2021-12-09 22:48:25 +0200

    Update prediction module
    Now weights downloads via URL

--- src/models/predict.py
+++ src/models/predict.py
@@ -5,7 +5,6 @@ by your image
 """
 
 from torchvision import transforms, models
-from PIL import Image
 from typing import Optional, Tuple
 import torch.nn as nn
 import torch
@@ -14,18 +13,22 @@ import torch
 class Classifier:
     def __init__(
         self,
-        weights_path: Optional[str] = None,
+        weights_url: Optional[str] = None,
         img_size: Optional[Tuple[int]] = (128, 128),
     ) -> None:
         """
         Args:
             weights_path:
-                path to weights of the model
+                url to weights of the model
             img_size:
                 size in which image would be resized as input for the model
         """
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.model = self._model_upload(weights_path)
+        
+        if weights_url is None:
+            weights_url = "https://github.com/roykoand/GoWinnerPrediction/releases/download/version(-1)/DenseNet121_weights.h5"
+        
+        self.model = self._model_upload(weights_url)
         self.model.to(self.device)
         self.preprocess = transforms.Compose(
             [
@@ -35,9 +38,7 @@ class Classifier:
             ]
         )
 
-    def _model_upload(
-        self, weights_path: Optional[str] = None
-    ) -> models.densenet.DenseNet:
+    def _model_upload(self, weights_url: str) -> models.densenet.DenseNet:
         """
         DenseNet121 model was the best at perfomance, so
         we will use it as the main model
@@ -46,15 +47,11 @@ class Classifier:
         """
         model = torch.hub.load("pytorch/vision:v0.10.0", "densenet121")
         model.classifier = nn.Linear(1024, 2, bias=True)
-        if weights_path is not None:
-            model.load_state_dict(
-                torch.load(weights_path, map_location=torch.device("cpu"))
-            )
+        model.load_state_dict(
+            torch.hub.load_state_dict_from_url(weights_url, progress=False, map_location=torch.device("cpu")))
         return model
 
     @torch.no_grad()
-    def predict(self, image_path: str) -> str:
-        image = Image.open(image_path).convert("RGB")
+    def predict(self, image) -> str:
         probs = torch.sigmoid(self.model(torch.unsqueeze(self.preprocess(image), 0)))
         return "W" if torch.argmax(probs) == 1 else "B"
-        
