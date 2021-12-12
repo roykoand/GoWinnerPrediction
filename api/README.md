c7848e0cd1779bee8980a8f2056da32ac28a3e29
roykoand <roykoand@gmail.com>
2021-12-13 00:32:59 +0200

    Update Go Winner Prediction API

--- api/README.md
+++ api/README.md
@@ -2,8 +2,9 @@
 
 Specify URL to your png image in the format (for example, use ```curl```):
 ```
-$ curl -X GET http://127.0.0.1:5000/?url=<URL_TO_IMAGE>
+$ curl -X GET http://127.0.0.1:5000/?url=<URL_TO_THE_IMAGE>
 {
-    "<URL_TO_IMAGE>": "<COLOR_OF_WINNER>"
+    "prediction": "<COLOR_OF_THE_WINNER>",
+    "url": "<URL_TO_THE_IMAGE>"
 }
 ```
--- api/api.py
+++ api/api.py
@@ -7,7 +7,7 @@ import sys
 
 sys.path.append(os.path.join(os.getcwd(), "src", "models"))
 
-from flask import Flask
+from flask import Flask, jsonify
 from PIL import Image
 from flask_restful import Resource, Api, reqparse
 from predict import Classifier  # :(
@@ -30,19 +30,19 @@ class GoWinnerPredictor(Resource):
         args = parser.parse_args()
 
         if args["url"].split(".")[-1].lower() not in ALLOWED_EXTENSIONS:
-            return {"Error": "Images must be in .png format"}
+            return jsonify(error="Images must be in .png format")
 
         try:
             responese = requests.get(args["url"], timeout=3)
             responese.raise_for_status()
         except:
-            return {"Error": "Error with connection to image url."}
+            return jsonify(error="Error with connection to image url.")
 
         img = np.array(bytearray(responese.content), dtype="uint8")
         img = cv2.imdecode(img, cv2.IMREAD_COLOR)
         img = Image.fromarray(img).convert("RGB")
 
-        return {args["url"]: Classifier().predict(img)}
+        return jsonify(url=args["url"], prediction=Classifier().predict(img))
 
 
 api.add_resource(GoWinnerPredictor, "/")
